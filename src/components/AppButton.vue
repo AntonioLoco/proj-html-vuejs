a8dd6a16fcd681e21acda8c8bebb43d123c10e4d
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-11-17 14:46:08 +0100

    Added jumbotron

---
+++ src/components/AppButton.vue
@@ -0,0 +1,40 @@
+<script>
+export default{
+    name: "AppButton",
+    props: {
+        name: String,
+        href: String,
+        color: String
+    }
+}
+</script>
+
+<template>
+    <a
+    class="ms_btn"
+    :href="href" 
+    :class="color"
+    >
+        {{name}}
+    </a>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+.ms_btn{
+    display: inline-block;
+    color: $text-white;
+    padding: .5em 1.5em;
+    border-radius: 20px;
+    text-align: center;
+
+    &.blue{
+        background: $background-btn-blue;
+    }
+
+    &.orange{
+        background: $background-btn-orange;
+    }
+}
+</style>
--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -1,14 +1,47 @@
 <script>
+// Import Component
+import AppButton from './AppButton.vue';
+
 export default{
-    name: "AppJumbotron"
+    name: "AppJumbotron",
+    components: {
+        AppButton
+    }
 }
 </script>
 
 <template>
     <!-- Jumbotron -->
-    <h1>Sono Jumbotron</h1>
+    <div class="jumbotron">
+        <div class="row row-cols-1 row-cols-lg-2 align-items-center pb-5">
+            <div class="col order-1 order-lg-0 pe-5">
+                <h1 class="fs-1 fw-bold">Business Planning</h1>
+                <h3 class="fs-2 fw-semibold">Digital Technology</h3>
+                <h2 class="fs-1 fw-bold">Modern Solution!</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis, reprehenderit!</p>
+                <AppButton name="Try it Now" href="/trynow" color="orange" class="me-3"/>
+                <AppButton name="Explore More" href="/exploremore" color="blue"/>
+            </div>
+            <div class="col order-0 order-lg-1 mb-3 mb-lg-0">
+                <img src="../assets/img/header.png" alt="Jumbo photo">
+            </div>
+        </div>
+    </div>
     <!-- /Jumbotron -->
 </template>
 
 <style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+.jumbotron{
+    p{
+        width: 60%;
+        font-size: .8rem;
+        color: $text-blue;
+        margin: 2em 0;
+    }
+
+    img{
+        max-width: 90%;
+    }
+}
 </style>
