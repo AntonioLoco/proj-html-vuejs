7541d925be27d1aa02a73711a3498a4008414e3f
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-11-21 17:44:41 +0100

    Added hover on service card

--- src/components/AppCardService.vue
+++ src/components/AppCardService.vue
@@ -21,32 +21,47 @@ export default{
         <div class="card-service-text">
             <h3>{{service.name}}</h3>
             <p class="fw-light">{{service.description}}</p>
+            <a href="" class="ms_btn">
+                <i class="fa-solid fa-arrow-right"></i>
+            </a>
         </div>
     </div>
 
 </template>
 
 <style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
 .card-service{
     width: 100%;
+    max-width: 300px;
     height: 100%;
     margin-bottom: 2em;
     display: flex;
     flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
     &-photo{
-        height: 40%;
+        width: 50%;
+        border-radius: 20px;
+        padding: 1em;
+        transform: translateY(30%);
+        transition: all .5s ease-out;
+        position: relative;
+        z-index: 10;
         img{
             width: 100%;
+            max-width: 150px;
             height: 100%;
             object-fit: contain;
         }
     }
 
     &-text{
-        height: 60%;
-        padding-top: 1.5em;
-        padding-bottom: 1em;
+        border-radius: 20px;
+        padding: 3.5rem 1em 1em;
+        position: relative;
+
         h3{
             font-size: 1.5rem;
             margin-bottom: 1em;
@@ -55,6 +70,40 @@ export default{
         p{
             font-size: .8rem;
         }
+
+        .ms_btn{
+            display: none;
+            width: 45px;
+            height: 45px;
+            text-align: center;
+            line-height: 45px;
+            border-radius: 50%;
+            background: $background-btn-orange;
+            color: $text-white;
+            position: absolute;
+            bottom: -42px;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            &:hover{
+                transition: all .5s ease-in-out;
+            }
+        }
+    }
+
+    &:hover .card-service-text{
+        color: $text-white;
+        background: $background-card-blog;
+        transition: all .2s ease-out;
+    }
+
+    &:hover .card-service-photo{
+        box-shadow: 6px 8px 16px 1px rgba(0,0,0,0.43);
+        background-color: $background-white;
+    }
+
+    &:hover .card-service-text .ms_btn{
+        display: inline-block;
+        transition: all .2s ease-out;
     }
 }
 </style>
--- src/components/AppServices.vue
+++ src/components/AppServices.vue
@@ -31,12 +31,12 @@ export default{
                 title="What We Do" 
                 paragraph="When, while the lovely valley teems with vapour around meand the"
                 color="black"
-                class="pb-5"
+                class="pb-2"
             />
 
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 py-5">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 pb-5">
                 <div 
-                    class="col px-5" 
+                    class="col px-4 d-flex align-items-center justify-content-center" 
                     v-for="(service,index) in store.ourServices"
                     :key="index"
                 >
