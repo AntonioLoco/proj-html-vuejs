53036cc95012e07ddcd8fc62204765180f28e5ea
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-11-22 15:21:46 +0100

    Added custom button in slider

--- src/components/AppSlider.vue
+++ src/components/AppSlider.vue
@@ -30,7 +30,10 @@ export default{
         :slidesPerView="1"
         :spaceBetween="10"
         :rewind="true" 
-        :navigation="true"
+        :navigation="{
+            nextEl: '.nextBtn',
+            prevEl: '.prevBtn'
+        }"
         :pagination="{
             clickable: true,
         }"
@@ -51,6 +54,14 @@ export default{
         :modules="modules"
         class="mySwiper"
     >
+        <div class="mySwiper-navigation">
+            <div class="prevBtn">
+                <i class="fa-solid fa-arrow-left"></i>
+            </div>
+            <div class="nextBtn">
+                <i class="fa-solid fa-arrow-right"></i>
+            </div>
+        </div>
         <swiper-slide v-for="(project,index) in projects" :key="index">
             <div class="carousel-card"> 
                 <img :src="getImage(`../assets/img/${project.img}.jpg`)" :alt="project.name">
@@ -108,6 +119,27 @@ export default{
 .mySwiper{
     overflow: visible;
     padding: 2em 0;
+
+    &-navigation{
+        position: absolute;
+        top: -40px;
+        right: 10px;
+        display: flex;
+
+        .prevBtn,
+        .nextBtn{
+            margin: 0 .5rem;
+            width: 35px;
+            height: 35px;
+            border: 1px solid $text-orange;
+            color: $text-orange;
+            border-radius: 50%;
+            display: flex;
+            cursor: pointer;
+            align-items: center;
+            justify-content: center;
+        }
+    }
 }
 
 .swiper-slide{
